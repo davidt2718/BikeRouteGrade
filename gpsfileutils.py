ad6d6c8f7748f2bad01f925fcd4fc16c23874135
David Towey <dtowey@apple.com>
2024-03-19 11:35:45 -0700

    rename to handle all types of gps files

--- gpsfileutils.py
+++ gpsfileutils.py
@@ -1,12 +1,15 @@
 '''Module to extract data from GPX files'''
+# NOTE If there is time/velocity data with the GPX, can probably use that to clean the data a bit
 
 import gpxpy  # GPX parser library  https://pypi.org/project/gpxpy/
 
 def readGpx(filepath):
     """Just read GPX in to a data file"""
+    # Lat, Long, Alt, Time, Speed?  should be enough for processing/filtering GPS data
     pass
 
 
 def getPositionFromGpx():
     """Get Lat, Long, Altitude data from GPX file and put in to a convenient format"""
     pass
+
