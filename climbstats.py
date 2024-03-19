a63393d81e22cc8177b3e7e96278a6f7f4c63a65
David Towey <dtowey@apple.com>
2024-03-19 11:36:11 -0700

    module to evaluate a climb segment

---
+++ climbstats.py
@@ -0,0 +1,7 @@
+'''Functions to characterize climbs by various metrics'''
+
+# Slope/Distance
+# Elevation-At-Grade (from detailed profile)
+# UCI Category (from distance/elev)
+# Overall hardness (taking in to account altitude, etc)
+
